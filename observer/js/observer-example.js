3725be0288ac1882d814ed60a772b9f8d9e4982e
Diego Pagini <diego.pagini@neoris.com>
2022-10-20 08:58:39 +0200

    observer example

--- observer/js/observer-example.js
+++ observer/js/observer-example.js
@@ -23,5 +23,50 @@ class Subject {
 class ItemSubject extends Subject {
 	constructor() {
 		super();
+
+		this.data = [];
+	}
+
+	add(item) {
+		this.data.push(item);
+		this.notify(this.data);
 	}
 }
+
+class HtmlElementObserver {
+	constructor(element) {
+		this.element = element;
+	}
+
+	refresh(data) {
+		this.element.innerHTML = data.reduce((acc, el) => {
+			return acc + `<p>${el}</p>`;
+		}, '');
+	}
+}
+
+class Observer {
+	constructor(fn) {
+		this.fn = fn;
+	}
+
+	refresh(data) {
+		this.fn(data);
+	}
+}
+
+const items = new ItemSubject();
+const div1Observer = new HtmlElementObserver(div1);
+const div2Observer = new HtmlElementObserver(div2);
+const div3Observer = new Observer((data) => {
+	div3.innerHTML = data.length;
+});
+
+items.subscribe(div1Observer);
+items.subscribe(div2Observer);
+items.subscribe(div3Observer);
+
+const add = () => {
+	const name = txtName.value;
+	items.add(name);
+};
